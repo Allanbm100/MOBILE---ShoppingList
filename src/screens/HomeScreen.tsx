14699bc6b3816c07ac80b84c39cb4e29da1350bc
Allanbm100 <allan.bm.100@gmail.com>
2025-10-15 20:54:40 -0300

    Using firebase storage

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,11 +1,15 @@
 import { useEffect, useState, useLayoutEffect } from 'react';
-import { Button, FlatList, StyleSheet, Text, TextInput, TouchableOpacity, View } from 'react-native';
-import { db } from '../config/firebase';
+import { Button, FlatList, Pressable, StyleSheet, Text, TextInput, TouchableOpacity, View } from 'react-native';
+import { db, storage } from '../config/firebase';
 import { collection, doc, addDoc, deleteDoc, updateDoc, onSnapshot } from 'firebase/firestore';
 import MaterialIcons from '@expo/vector-icons/MaterialIcons';
 import Dialog from 'react-native-dialog';
 import { signOut } from 'firebase/auth';
 import { auth } from '../config/firebase';
+import * as ImagePicker from 'expo-image-picker'
+import { manipulateAsync, SaveFormat } from 'expo-image-manipulator'
+import { Image } from 'expo-image'
+import { getDownloadURL, ref, uploadBytes } from 'firebase/storage';
 
 export default function HomeScreen({ navigation }: { navigation: any }) {
     const [productName, setProductName] = useState("");
@@ -14,8 +18,14 @@ export default function HomeScreen({ navigation }: { navigation: any }) {
     const [selectedProductName, setSelectedProductName] = useState("");
     const [selectedProductId, setSelectedProductId] = useState("");
 
+    const [showImage, setShowImage] = useState(false);
+    const [selectedImage, setSelectedImage] = useState("");
+
     const renderItem = ({ item }: { item: any }) => (
         <View style={styles.itemContainer}>
+            <Pressable onPress={() => presentImage(item.image)}>
+                <Image source={{ uri: item.image }} style={{ width: 80, height: 80, borderRadius: 6 }} />
+            </Pressable>
             <Text style={styles.itemText}>{item.name}</Text>
             <TouchableOpacity onPress={() => showDialog(item)}>
                 <MaterialIcons name="edit" size={24} color="#6565cfff" />
@@ -43,8 +53,21 @@ export default function HomeScreen({ navigation }: { navigation: any }) {
     // }
 
     const addProduct = async () => {
+        if (productName.trim() === "") {
+            setProductName("");
+            return;
+        }
+        const uri = await pickMedia();
         try {
-            const docRef = await addDoc(collection(db, "ShoppingList"), { name: productName });
+            var imageUri = null;
+            if (uri) {
+                const fileName = uri.split("/").pop();
+
+                const compressedUri = await compressImage(uri);
+                imageUri = await uploadMediaAsync(compressedUri, `uploads/${fileName}`);
+
+            };
+            const docRef = await addDoc(collection(db, "ShoppingList"), { name: productName, image: imageUri });
             // setProducts(prev => [...prev, { id: docRef.id, name: productName }]);
             setProductName("");
         } catch (error) {
@@ -86,6 +109,42 @@ export default function HomeScreen({ navigation }: { navigation: any }) {
         } catch (error) {
             console.log("Erro ao sair: ", error);
 
+        }
+    }
+
+    const pickMedia = async () => {
+        const result = await ImagePicker.launchImageLibraryAsync({
+            mediaTypes: ['images'],
+            quality: 1,
+            //allowsEditing: true (Caso o usuário possa editar a edição da imagem)
+        });
+        if (!result.canceled) return result.assets[0].uri;
+        else return null;
+    }
+
+    const compressImage = async (uri:string) => {
+        const manipulated = await manipulateAsync(
+            uri,
+            [{ resize: { width: 400 } }],
+            { compress: 0.6, format: SaveFormat.WEBP }
+        );
+        return manipulated.uri;
+    }
+
+    const uploadMediaAsync = async (uri: string, path: string) => {
+        const response = await fetch(uri);
+        const blob = await response.blob();
+        const fileRef = ref(storage, path);
+        await uploadBytes(fileRef, blob);
+
+        const downloadURL = await getDownloadURL(fileRef);
+        return downloadURL;
+    }
+
+    const presentImage = (image: any) => {
+        if (image) {
+            setSelectedImage(image);
+            setShowImage(true);
         }
     }
 
@@ -136,6 +195,12 @@ export default function HomeScreen({ navigation }: { navigation: any }) {
                 <Dialog.Button label="Cancelar" onPress={() => setVisible(false)} />
                 <Dialog.Button label="Atualizar" onPress={handleUpdate} />
             </Dialog.Container>
+
+            {showImage && (
+                <Pressable onPress={() => setShowImage(false)} style={ styles.overlay }>
+                    <Image source={{uri: selectedImage }} style={ styles.fullImage } contentFit='contain' />
+                </Pressable>
+            )}
         </View>
     )
 }
@@ -192,5 +257,20 @@ const styles = StyleSheet.create({
         color: '#333333',
         width: '100%',
         flex: 1,
-    }
+    },
+    fullImage: {
+        width: '90%',
+        height: '80%',
+        borderRadius: 10
+    },
+    overlay: {
+        position: 'absolute',
+        top: 0,
+        bottom: 0,
+        left: 0,
+        right: 0,
+        backgroundColor: 'rgba(0, 0, 0, 0.8)',
+        justifyContent: 'center',
+        alignContent: 'center'
+    },
 });
